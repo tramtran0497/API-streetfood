2a09e932079285f5652f6b1c69618bf73191df92
tramtran0497 <tramtran0497@gmail.com>
2022-05-02 22:37:59 +0300

    create demo CRUD in product router

--- src/Router/productRouter.js
+++ src/Router/productRouter.js
@@ -1,8 +1,47 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/products", (req, res) => {
-    res.send("All products")
+router.get("/products", async(req, res) => {
+    try{
+        res.send("All products")
+    }catch(error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.get("/product/:id", async(req, res) => {
+    const {id} = req.params
+    try{
+        res.send("A product")
+    }catch(error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.post("/products", async(req, res) => {
+    try{
+        res.send("Create a product")
+    }catch(error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.put("/product/:id", async(req, res) => {
+    const {id} = req.params
+    try{
+        res.send("Change a product")
+    }catch(error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.delete("/product/:id", async(req, res) => {
+    const {id} = req.params
+    try{
+        res.send("Delete a product")
+    }catch(error) {
+        res.status(400).send(error.message)
+    }
 })
 
 module.exports = router
