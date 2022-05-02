2980bd52422f3218b562f54e8454b8af4ce26d00
tramtran0497 <tramtran0497@gmail.com>
2022-05-02 22:13:01 +0300

    Server is up on port and create user router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const express = require("express")
+require('dotenv').config()
+const port = process.env.PORT
+const productRouter = require("./Router/productRouter")
+const app = express()
+
+app.use(productRouter)
+
+app.listen(port, () => {
+    console.log("Server is up on " + port)
+})
